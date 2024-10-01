629f90ab58385a5dc08b4659768d06ee98ef07d5
prajwal lama <prajwallama28@gmail.com>
2024-10-01 15:00:50 +0545

    New

---
+++ FormulaOne.chat/FormulaOne.chat/DataService/SharedDb.cs
@@ -0,0 +1,12 @@
+﻿using FormulaOne.chat.Models;
+using System.Collections.Concurrent;
+
+namespace FormulaOne.chat.DataService
+{
+    public class SharedDb
+    {
+        private readonly ConcurrentDictionary<string, UserConnection> _connections=new();
+        public ConcurrentDictionary<string,UserConnection> connections=> _connections;
+    }
+
+}
---
+++ FormulaOne.chat/FormulaOne.chat/Program.cs
@@ -0,0 +1,38 @@
+using FormulaOne.chat.DataService;
+using FormulaOne.chat.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
+builder.Services.AddSingleton<SharedDb>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseRouting();
+app.MapControllers();
+app.UseEndpoints(endpoints =>
+{
+   
+    endpoints.MapHub<ChatHub>("/Chat");
+});
+//app.MapHub<ChatHub>("/Chat");
+
+
+app.Run();
